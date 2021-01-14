aa53a18e6c203d7b017ef986c7743b2b28820f29
Erik Brown <ejbrownlf@gmail.com>
2021-01-13 18:19:18 -0600

    jokes url variable and commented commands for jokesAPI url

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var jokesURL = "https://v2.jokeapi.dev/joke/Any?"
+// Jokes commands:
+// Format: ?format=format i.e json, xml, yaml, txt
+// Blacklist Flags: ?blacklistFlags=flag1[,flag2,...] i.e. nsfw, religious, political, racist, sexist, explicit
+// Joke type: ?type=type i.e. single, twopart
+// Joke amount: ?amount=number
