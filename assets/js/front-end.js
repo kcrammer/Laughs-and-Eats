098886a1e37cede12c69516e6fd86b8f64ac45c1
kcrammer <kcrammer@gmail.com>
2021-01-20 19:28:07 -0500

    added show more/show less buttons to front-end js file

--- assets/js/front-end.js
+++ assets/js/front-end.js
@@ -15,3 +15,19 @@ $("#food-more").on("click", function(event){
         showMore = false;
     }
 })
+
+$("#joke-more").on("click", function(event) {
+    event.preventDefault();
+    console.log("click");
+    if(!showMore){
+        console.log("yay")
+        $(".hidden-element").removeClass("hide");
+        $("#joke-more").text("Show Less");
+        showMore = true;
+    }
+    else{
+        $(".hidden-element").addClass("hide");
+        $("#joke-more").text("Show More");
+        showMore = false;
+    }
+})
